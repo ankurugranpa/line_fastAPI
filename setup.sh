874795a1938aac2cf000b51d3afbd1f5890a16b2
ankuru <yuki.tori.39@gmail.com>
2023-09-15 21:06:42 +0900

    [fix] setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-mkdir .env
-echo "LINE_CHANEL_API_KEY='your_api_key'" > .env
-echo "LINE_USER_ID='your_user_id'" > .env
+touch .env
+echo "LINE_CHANEL_API_KEY='your_api_key'" >> .env
+echo "LINE_USER_ID='your_user_id'" >> .env
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
