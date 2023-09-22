d41cf8ba4076c1b036418d4869427744a06218c7
ankuru <yuki.tori.39@gmail.com>
2023-09-22 13:44:10 +0900

    Feat: connect to Line message api
    this program can connect line message api,

--- main.py
+++ main.py
@@ -5,60 +5,71 @@ from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
 from typing import Union
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
-import requests
+from fastapi import  FastAPI, Request, Header
 
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import (
+        Configuration,
+        ApiClient,
+        MessagingApi,
+        ReplyMessageRequest,
+        TextMessage
+        )
 
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+from starlette.exceptions import HTTPException
+
+# lineボット用の設定
 CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANEL_API_KEY')
 USER_ID = os.environ.get('LINE_USER_ID')
+CHANNEL_SECLET = os.environ.get('LINE_CANNEL_SECLET')
 
-app = FastAPI()
-
-
-origins = [
-    "http://localhost:8601",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECLET)
 
 
-def send_notify(message):
-    url = "https://notify-api.line.me/api/notify"
-    token = "Iz5Kax5Z8h5XrRCxFDbZVxsMMQAbrT1JIPiya37J0cT"
-    headers = {"Authorization": "Bearer " + token}
-
-    # message = 'message送信！'
-    payload = {"message":  message}
-
-    requests.post(url, headers=headers, params=payload)
+app = FastAPI(title="linebot-sample", description="connect scratch")
 
 
-def send_message(text):
-    line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-    print("test")
+# Root
+@app.get("/")
+def root():
+    return {"title": app.title, "description": app.description}
+
+
+# Line Webhook
+@app.post("/callback")
+async def callback(request: Request, x_line_signature=Header(None)):
+
+    body = await request.body()
+
     try:
-        line_bot_api.push_message(USER_ID, TextSendMessage(text=text))
-    except LineBotApiError as e:
-        print(e.message)
+        handler.handle(body.decode("utf-8"), x_line_signature)
+
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="InvalidSignatureError")
+
+    return "OK"
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: str):
-    # send_notify(item_id)
-    item = {"item_id": item_id}
-    print("test")
-    text = "これはline message apiを使用して送信しています。" + item_id
-    send_message(text)
-    # if q:
-    #     item.update({"q": q})
-    # if not short:
-    #     item.update(
-    #         {"description": "This is an amazing item that has a long description"}
-    #     )
-    return item
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        text = event.message.text
+        if text == "ちんちん":
+            text = "同じです"
+        else:
+            text = "違います"
+        # print(event)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                # messages=[TextMessage(text=event.message.text)]
+                messages=[TextMessage(text=text)]
+            )
+        )
