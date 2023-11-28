9e7ed847422676628acc2e2c5c017b75e3a0c7d8
ankuru <ankuru.ahahahaha@gmail.com>
2023-11-29 02:45:16 +0900

    update:[Can not use pip on setup.sh]
    We do not use pip, so rm something pip script on setup.sh and Rm
    requirement.txt

--- setup.sh
+++ setup.sh
@@ -2,6 +2,3 @@ touch .env
 echo "LINE_CHANEL_API_KEY='your_api_key'" >> .env
 echo "LINE_USER_ID='your_user_id'" >> .env
 echo "LINE_CANNEL_SECLET='your_channel_seclet'" >> .env
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
