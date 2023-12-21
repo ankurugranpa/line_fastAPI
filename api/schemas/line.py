3d028e48728159657a8b1d62bd8bfa5473ca519e
ankuru <ankuru.ahahahaha@gmail.com>
2023-12-21 14:43:31 +0900

    add:[Add api schemas for line api]
    line message apiとつなぐためのschemaを作成した。Image, Video,
    Audioの送信用スキーマを作成した

--- api/schemas/line.py
+++ api/schemas/line.py
@@ -4,32 +4,32 @@ from pydantic import BaseModel, Field
 
 
 class LineBase(BaseModel):
-    user_id: str
+    user_id: str = Field(description="Line User Id")
 
 class LineResponseBase(BaseModel):
-    status: int
+    status: int = Field(description="callback status")
 
 
 class LineSendText(LineBase):
-    message: str
+    message: str = Field(description="Text Message @size: Max 5000word")
 
 class LineSendAudio(LineBase):
-    audio_url: str
-    duration: int
+    audio_url: str = Field(description="Audio Url @type: mp3, m4a @size Max200MB")
+    duration: int = Field(description="Audio content length @type ms")
+
+class LineSendImage(LineBase):
+    image_url: str = Field(description="Image Url @type: jpeg, png @size: Max10MB")
+    preview_image_url: str = Field(description="Preview Image URL @type: jpeg, png @size: Max1MB")
+
+class LineSendVideo(LineBase):
+    video_url: str = Field(description="Video URL @type: mp4 @size: Max200MB")
+    preview_video_url: str = Field(description="Preview Video URL @type: jpeg, png @size: Max1MB")
 
 
-class LineSendTextResponse(LineSendText):
+class LineSendResponse(LineSendText):
     status: int
 
 
-class LineSendAudioResponse(LineSendAudio):
-    status: int
-
-    """
-    content: [str] = Field(None, example="自己紹介してください")
-    prompt: Optional[str] = Field(default="You are a helpful assistant.")
-    temperature: Optional[float] = Field(default=0.7)
-    """
 class LineGetMessage(LineBase):
     message: str
     class Config:
